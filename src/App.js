219f76c77434fef6864a470f294e4c30e3973342
sefammirza <sefammirza@gmail.com>
2022-05-21 21:49:00 +0200

    headerStyled

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="app">
+     <Router>
+      <>
+        <Switch>
+          <Route path="/" exact>
+            <Header/>
+          </Route>
+        </Switch>
+      </>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC8K_kVouuWYpIGvfJngTTOO-X5SQrx01k",
+    authDomain: "slack-clone-4e9be.firebaseapp.com",
+    projectId: "slack-clone-4e9be",
+    storageBucket: "slack-clone-4e9be.appspot.com",
+    messagingSenderId: "776815951841",
+    appId: "1:776815951841:web:947dc6441bed8e21d15ca0"
+  };
